0158f23143af3e96add20a5206bd0c91c4619c04
hjvm2718 <mettenheim@acantoresearch.com>
2016-12-09 09:42:05 +0100

    Update model.rb

--- lib/ib/model.rb
+++ lib/ib/model.rb
@@ -4,5 +4,6 @@ require 'ib/base'
 module IB
   # IB Models can be either lightweight (tableless) or database-backed.
   # require 'ib/db' - to make all IB models database-backed
-  Model =  IB.db_backed? ? ActiveRecord::Base : IB::Base
+  #Model =  IB.db_backed? ? ActiveRecord::Base : IB::Base
+  Model = IB::Base
 end
